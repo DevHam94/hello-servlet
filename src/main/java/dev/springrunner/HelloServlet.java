34574a432c334f7cee60bd0844e8364a2bd2fe9e
admin <gkaeogus7@gmail.com>
2023-08-22 11:29:43 +0900

    20230822

--- src/main/java/dev/springrunner/HelloServlet.java
+++ src/main/java/dev/springrunner/HelloServlet.java
@@ -20,6 +20,15 @@ public class HelloServlet extends HttpServlet {
 
         // 3. 두 이름정보를 바탕으로 html 을 작성하는 것
         StringBuilder html = new StringBuilder();
+        html.append("<html>");
+        html.append("   <head>");
+        html.append("       <title>초 간단 서블릿 개발하기</title>");
+        html.append("   </head>");
+        html.append("   <body>");
+        html.append("       <h1>" + requestName + "님 안녕하세요.</h1>");
+        html.append("       <h1>저는 " + servletName + "입니다.</h1>");
+        html.append("   </body");
+        html.append("</html>");
 
         // 4. HTTP 응답을 작성하는 것 (상태코드, 헤더, 응답 콘텐트)
         response.setStatus(200);
