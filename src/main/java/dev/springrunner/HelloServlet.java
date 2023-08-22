1c2d7a4699ab70a9c5ed2467cad8de710440d7ca
admin <gkaeogus7@gmail.com>
2023-08-22 11:27:12 +0900

    Initial commit

---
+++ src/main/java/dev/springrunner/HelloServlet.java
@@ -0,0 +1,30 @@
+package dev.springrunner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = { "/hello" })
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // 1. 요청 데이터로부터 이름정보를 꺼내는 것
+        String requestName = request.getParameter("name");
+        // 2. 실행하는 서블릿에 이름정보를 찾는 것
+        String servletName = getServletConfig().getServletName();
+
+        // 3. 두 이름정보를 바탕으로 html 을 작성하는 것
+        StringBuilder html = new StringBuilder();
+
+        // 4. HTTP 응답을 작성하는 것 (상태코드, 헤더, 응답 콘텐트)
+        response.setStatus(200);
+        response.setContentType("text/html; charset=UTF-8");
+        response.getWriter().write(html.toString());
+
+    }
+}
